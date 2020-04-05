7c17cf4175725f70b4122d3b98a0f1869cac0228
Silvia Rocha <silviadprocha@gmail.com>
2020-04-05 19:11:16 +0900

    add render class badge dynamically

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,11 +8,17 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.FormatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.FormatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   FormatCount() {
     const { count } = this.state;
     // return this.state.count === 0 ? "Zero" : this.state.count;
