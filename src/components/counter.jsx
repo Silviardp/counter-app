451e175c150d9cc9f04b0d62a08b685772681a08
Silvia Rocha <silviadprocha@gmail.com>
2020-04-07 18:58:37 +0900

    pass data to counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: [],
   };
 
@@ -15,7 +15,7 @@ class Counter extends Component {
     // add an arrow function instead of constructor
     // console.log("Increment clicked", this);
     console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   renderTags() {
@@ -47,14 +47,14 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   FormatCount() {
-    const { count } = this.state;
-    // return this.state.count === 0 ? "Zero" : this.state.count;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    // return this.state.value === 0 ? "Zero" : this.state.value;
+    return value === 0 ? "Zero" : value;
   }
 }
 
